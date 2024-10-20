3432b927d74c73d1c794d5c0751b27bb8082a204
xxxw1tnessbtwxxx <work.w1tnessbtw@mail.ru>
2024-10-20 21:54:28 +0700

    new comment

--- SpeedSolverAPI/SpeedSolverCore/DTO/Chat/ProjectMessageCreateDto.cs
+++ SpeedSolverAPI/SpeedSolverCore/DTO/Chat/ProjectMessageCreateDto.cs
@@ -2,6 +2,7 @@
 {
     public class ProjectMessageCreateDto
     {
+        // dto for api
         public string Content { get; set; }
         public int UserId { get; set; }
         public int ProjectId { get; set; }
