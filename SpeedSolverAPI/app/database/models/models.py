10a182773bdca006777a106adcd7ca969115cc8f
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2024-11-21 22:12:39 +0700

    inited frontend

--- SpeedSolverAPI/app/database/models/models.py
+++ SpeedSolverAPI/app/database/models/models.py
@@ -1,13 +1,17 @@
 import datetime
 from typing import List
-from sqlalchemy import Column, Integer, String, Boolean, Date
+
+import uuid
+
+from sqlalchemy import Column, Integer, String, Boolean, Date, BigInteger
 from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship, mapped_column, Mapped, declarative_base
 
 Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
-    userId: Mapped[int] = mapped_column(primary_key=True)
+    userId: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     login: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
     registered: Mapped[datetime.date] = mapped_column(Date, nullable=False, default=datetime.date.today())
@@ -16,37 +20,51 @@ class User(Base):
 
 class UserProfile(Base):
     __tablename__ = "profiles"
-    userProfileId: Mapped[int] = mapped_column(primary_key=True)
+    userProfileId: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.userId"), nullable=False)
     user: Mapped[User] = relationship("User", back_populates="profile")
     
 
 class Team(Base):
     __tablename__ = "teams"
-    teamId: Mapped[int] = mapped_column(primary_key=True)
+    teamId: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     teamName: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     teamDescription: Mapped[str] = mapped_column(String(255), nullable=False)
     createdAt: Mapped[Date] = mapped_column(Date, nullable=False, default=datetime.date.today())
     creatorId: Mapped[int] = mapped_column(ForeignKey("users.userId"), nullable=False)
-    creator: Mapped["User"] = relationship("User", back_populates="team")
+    creator: Mapped["User"] = relationship("User", back_populates="teams")
+
+    projects: Mapped["TeamProject"] = relationship("TeamProject")
 
 class GeneralObjective(Base):
     __tablename__ = "general_objectives"
-    generalObjectiveId: Mapped[int] = mapped_column(primary_key=True)
+    generalObjectiveId: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     generalObjectiveName: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     generalObjectiveDescription: Mapped[str] = mapped_column(String(255), nullable=False)
     underObjectives: Mapped[List["UnderObjective"]] = relationship("UnderObjective", back_populates="generalObjective")
 
 class UnderObjective(Base):
     __tablename__ = "under_objectives"
-    underObjectiveId: Mapped[int] = mapped_column(primary_key=True)
+    underObjectiveId: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     underObjectiveName: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     underObjectiveDescription: Mapped[str] = mapped_column(String(255), nullable=False)
     generalObjectiveId: Mapped[int] = mapped_column(ForeignKey("general_objectives.generalObjectiveId"), nullable=False)
     generalObjective: Mapped["GeneralObjective"] = relationship("GeneralObjective", back_populates="underObjectives")
 
-class TeamObjective(Base):
-    __tablename__ = "team_objectives"
-    teamObjectiveId: Mapped[int] = mapped_column(primary_key=True)
-    generalObjectiveId: Mapped[int] = mapped_column(ForeignKey("general_objectives.generalObjectiveId"), nullable=False)
-    generalObjective: Mapped["GeneralObjective"] = relationship("GeneralObjective")
+class Project(Base):
+    __tablename__ = "projects"
+    projectId: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    projectTitle: Mapped[str] = mapped_column(String(50), unique=True)
+    projectDescription: Mapped[str] = mapped_column()
+    tasks: Mapped[List["ProjectTask"]] = relationship("ProjectTask", back_populates="project")
+    
+class ProjectTask(Base): 
+    __tablename__ = "project_objectives"
+    projectTaskId: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    generalObjectiveId: Mapped[UUID] = mapped_column(ForeignKey("general_objectives.generalObjectiveId"))
+    projectId: Mapped[UUID] = mapped_column(ForeignKey("projects.projectId"))
+    project: Mapped["Project"] = relationship("Project", back_populates="tasks") 
+
+class TeamProject(Base):
+    __tablename__ = "team_projects"
+    teamProjectid: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
--- SpeedSolverDesktop/DesktopApp/obj/Debug/net8.0/DesktopApp.AssemblyInfo.cs
+++ SpeedSolverDesktop/DesktopApp/obj/Debug/net8.0/DesktopApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesktopApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ffbf052f408df0caaaf6803929b6af0ab4cd69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a2ac251e1c022e911eaf5cbf3198a3d183ad54")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesktopApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesktopApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
