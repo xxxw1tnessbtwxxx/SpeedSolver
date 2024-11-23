83b2de64c60abce766082b5b67568192f52f1e5b
xxxw1tnessbtwxxx <work.w1tnessbtw@mail.ru>
2024-11-24 00:27:45 +0700

    frontend docker

--- SpeedSolverFrontend/src/App.jsx
+++ SpeedSolverFrontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from 'antd'
-function App() {
+import React from 'react'
+
+import Header from './components/header/Header'
+import './App.css'
+export default function App() {
 
   return (
     <>
-      <div>
-        <Button type='primary'>asdasdsa</Button>
-      </div>
+      <Header/>
+      <div className="container">asdasdsa</div>
     </>
+    
   )
 }
 
-export default App
+
---
+++ SpeedSolverFrontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Divider } from 'antd';
+import styles from './Header.module.css'
+
+
+export default function Header() {
+    return (
+        <header className={styles.headerMain}>
+            <div className={styles.container}>
+                <h1>SpeedSolver</h1>
+                <div className={styles.headerButtons}>
+                    <Button type='primary'>Login</Button>
+                    <Button type='primary'>Register</Button>
+                </div>
+            </div>
+            <Divider/>
+        </header>
+    )
+}
