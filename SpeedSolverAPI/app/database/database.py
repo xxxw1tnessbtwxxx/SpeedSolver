d5051020905176fcef836ac24c72deb966ccdb13
xxxw1tnessbtwxxx <work.w1tnessbtw@mail.ru>
2024-11-24 14:08:30 +0700

    frontend work

--- SpeedSolverAPI/app/database/database.py
+++ SpeedSolverAPI/app/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.engine import create_engine
 from ..cfg.config import config
 
-async def get_session():
+async def get_session() -> Session:
     engine = create_engine(str(config.db_url))
     session = Session(bind=engine)
     return session
--- SpeedSolverFrontend/src/App.jsx
+++ SpeedSolverFrontend/src/App.jsx
@@ -1,12 +1,24 @@
 
 import Header from './components/Header/Header'
 import './App.css'
+import InfoCard from './components/InfoCard/InfoCard'
 export default function App() {
 
   return (
     <>
       <Header/>
-      <div className="container">asdasdsa</div>
+      <div className="container">
+        <div className="welcomeText">
+          <h2>Добро пожаловать в SpeedSolver</h2> 
+        </div>
+
+
+        <div className="cards">
+          <InfoCard title="Планируйте." description="Создавайте необходимые задачи."/>
+          <InfoCard title="Распределяйте." description="Назначайте сотрудников команды на проекты."/>
+          <InfoCard title="Контролируйте." description="Устанавливайте дедлайны."/>
+        </div>
+      </div>
     </>
     
   )
