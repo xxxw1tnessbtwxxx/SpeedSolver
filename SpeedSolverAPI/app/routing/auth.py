1f005d5a7bdfbd37aa69196a5ab84fb2b54286d3
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2024-11-24 10:49:27 +0700

    fixed session

--- SpeedSolverAPI/app/routing/auth.py
+++ SpeedSolverAPI/app/routing/auth.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from ..schema.authorize import Authorize
 from ..database.repo.user_repo import UserRepository
-
+from ..database.database import get_session
 authRouter = APIRouter(prefix="/auth", tags=["Auth"])
 
 @authRouter.post("/authorize")
 async def authorze(data: Authorize):
-    return await UserRepository().authorize(data)
+    return await UserRepository(get_session()).authorize(data)
