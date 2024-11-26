01f95c124c27a53fc389d35463ef016012188654
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2024-11-26 16:39:55 +0700

    hotfix: auto_migrate

--- SpeedSolverAPI/app/handlers/auto_migrate.py
+++ SpeedSolverAPI/app/handlers/auto_migrate.py
@@ -2,4 +2,4 @@ from alembic.config import Config
 from alembic import command
 
 def migrate():
-    command.upgrade(Config('../alembic.ini'), "head")
+    command.upgrade(Config("alembic.ini"), "head")
