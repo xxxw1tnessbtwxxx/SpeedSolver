fce5e985f651f04ba3e1ae69bf60dcd62901d2d2
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2024-12-10 15:52:54 +0700

    fixed jwt auth

--- SpeedSolverBackend/SpeedSolverAPI/app/utils/logger/logger.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/utils/logger/logger.py
@@ -1,7 +1,7 @@
 import logging
 from logging import Logger
 import datetime
-logging.basicConfig(level=logging.INFO, filename=f"logs/{datetime.date.today()}.log",filemode="w",
+logging.basicConfig(level=logging.INFO, filename=f"logs/{datetime.date.today()}.log",filemode="a",
                     format="%(asctime)s %(levelname)s %(message)s")
 
 logger = logging.getLogger(__name__)
