bda22f1f8c4825e15069a6dc39bb1f70dcf52cd7
xxxw1tnessbtwxxx <work.w1tnessbtw@mail.ru>
2024-12-10 11:10:20 +0700

    reloacting auth method

--- SpeedSolverBackend/SpeedSolverAPI/app/database/repo/user_repository.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/database/repo/user_repository.py
@@ -1,4 +1,7 @@
 from sqlalchemy.orm import Session
+
+from app.routing.security.hasher import verify_password
+from app.utils.result import Result
 from ..abstract.abc_repo import AbstractRepository
 from app.database.models.models import User
 
@@ -6,3 +9,11 @@ from sqlalchemy import select, update, insert
 
 class UserRepository(AbstractRepository):
     model = User
+
+    def authenticate_user(self, email: str, password: str) -> Result[None]:
+        user = UserRepository(self._session).get_by_filter_one(email=email)
+        if not user:
+            return Result(success=False, error="User not found")
+        if not verify_password(password, user.password):
+            return Result(success=False, error="Invalid password")
+        return Result(success=True, value=user)
