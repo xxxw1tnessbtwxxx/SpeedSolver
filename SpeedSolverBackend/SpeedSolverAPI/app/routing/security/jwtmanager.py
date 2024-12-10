4aad2cdc67f45dd316a2052b0de089a9483d2bd8
xxxw1tnessbtwxxx <work.w1tnessbtw@mail.ru>
2024-12-10 11:07:35 +0700

    added decoding, authenticate to jwtmanager

--- SpeedSolverBackend/SpeedSolverAPI/app/routing/security/jwtmanager.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/routing/security/jwtmanager.py
@@ -1,5 +1,13 @@
+from fastapi.security import OAuth2PasswordBearer
 import jwt
 from app.cfg.settings import settings
+from app.database.repo.user_repository import UserRepository
+from app.database.database import get_session
+from app.utils.result import Result
+from app.routing.security.hasher import verify_password
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 class JWTManager:
     
     def __init__(self):
@@ -9,5 +17,20 @@ class JWTManager:
 
     @classmethod
     def encode_token(self, payload: dict):
-        exp = payload.copy().update({"exp": self.EXPIRES_AT * 60})
-        return jwt.encode(payload, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        jwt_payload = payload.copy().update({"exp": self.EXPIRES_AT * 60})
+        return jwt.encode(jwt_payload, self.SECRET_KEY, algorithm=self.ALGORITHM)
+    
+    @staticmethod
+    def authenticate_user(email: str, password: str) -> Result[None]:
+        with get_session() as session:
+            user = UserRepository(session).get_by_filter_one(email=email)
+            if not user:
+                return Result(success=False, error="User not found")
+            if not verify_password(password, user.password):
+                return Result(success=False, error="Invalid password")
+            return Result(success=True, value=user)
+        
+    
+    @staticmethod
+    def decode_token(token: str) -> dict:
+        return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
