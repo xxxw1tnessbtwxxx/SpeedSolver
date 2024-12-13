6e71848b57bebeec8ec72a4f715f828ab2c73b3c
xxxw1tnessbtwxxx <work.w1tnessbtw@mail.ru>
2024-12-13 20:08:20 +0700

    revoking js to ts

--- SpeedSolverMVP/SpeedSolverFrontend/vite.config.ts
+++ SpeedSolverMVP/SpeedSolverFrontend/vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'dist'
-  }
 })
