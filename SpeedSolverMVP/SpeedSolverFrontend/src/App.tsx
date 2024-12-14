0a26e92f8e16f173cc7300961be45037dfca8460
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2024-12-14 13:08:41 +0700

    new login page, form, styles, self styles kit

--- SpeedSolverMVP/SpeedSolverFrontend/src/App.tsx
+++ SpeedSolverMVP/SpeedSolverFrontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import WelcomePage from "./pages/welcome/WelcomePage"
-
+import LoginPage from "./pages/login/LoginPage"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>  
   )
 }
