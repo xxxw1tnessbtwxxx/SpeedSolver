83363de89b7819ada8faab9f635eaf57d479dc8a
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2024-12-14 14:38:18 +0700

    added funcs auth and reg, catching errors

--- SpeedSolverMVP/SpeedSolverFrontend/src/pages/access/AccessPage.tsx
+++ SpeedSolverMVP/SpeedSolverFrontend/src/pages/access/AccessPage.tsx
@@ -1,17 +1,63 @@
-import React from "react"
+import React, { useState } from "react"
 import "../../anystyles/centeredContainer.css"
 import "../../anystyles/speedsolveruikit.css"
 import PrimaryButton from "../../components/primaryButton/PrimaryButton"
 
 import styles from "./AccessPage.module.css"
 import AuthorizationType from "../../types/enums/AuthorizationType"
+import axios from "axios"
+
+import API_URL from "../../types/api/api"
+import Swal from "sweetalert2"
+import { toast, ToastContainer } from "react-toastify"
 
 interface AuthTypeProp {
     action: AuthorizationType
 }
 
 
+
 const AccessPage: React.FC<AuthTypeProp> = ({ action }) => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function Register(email: string, password: string) {
+        
+        const data = {
+            'email': email,
+            'password': password
+        }
+
+        try {
+            var response = await axios.post(`${API_URL()}/access/register`, data)
+            return response
+        }
+        catch(error) {
+            throw error
+        }
+    }
+
+    async function Authorize(email: string, password: string) {
+
+        const data = {
+            'username': email,
+            'password': password
+        }
+
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        try {
+            var response = await axios.post(`${API_URL()}/access/authorize`, data, { headers: headers })
+            return response
+        }
+        catch(error) {
+            throw error
+        }
+    }
+
 
     const content = action === AuthorizationType.LOGIN ? "Авторизация" : "Регистрация"
     const text = action === AuthorizationType.LOGIN ? "Авторизоваться" : "Зарегистрироваться"
@@ -28,13 +74,30 @@ const AccessPage: React.FC<AuthTypeProp> = ({ action }) => {
                     </div>
 
                     <div className={styles.mainForm}>
-                        <input type="text" placeholder="Логин" className="defaultInput"/>
-                        <input type="password" placeholder="Пароль" className="defaultInput"/>
-                        <PrimaryButton text={text}/>
+                        <input type="text" placeholder="Логин" className="defaultInput" onChange={(e) => { setEmail(e.target.value) }}/>
+                        <input type="password" placeholder="Пароль" className="defaultInput" onChange={(e) => { setPassword(e.target.value) }}/>
+                        <PrimaryButton text={text} onClick={action === AuthorizationType.LOGIN ? 
+                            () => Authorize(email, password).then(res => {
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: res.data.access_token,
+                                    showConfirmButton: true,
+                                })
+                            }).catch(() => {
+                              toast.error("Что-то пошло не так.")  
+                            }) 
+                            : () => Register(email, password).then(() => {
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: "Вы успешно зарегистрировались в сервисе. \nСпасибо, что Вы с нами <3",
+                                    showConfirmButton: true,
+                                })
+                            }).catch(() => {toast.error(`Вероятно, такой пользователь уже существует :(`)})}/>
                     </div>
 
                 </div>
             </div>
+            <ToastContainer/>
         </>
     )
 }
